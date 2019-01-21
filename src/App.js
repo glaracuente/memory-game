f2cabe5aca9ccea861b827de472cc7ddd724b042
glaracuente <glaracuente@outbrain.com>
2019-01-21 00:02:00 -0500

    pics working basic

--- src/App.js
+++ src/App.js
@@ -1,29 +1,25 @@
-import React, { Component } from 'react';
+import React from 'react';
 //import logo from './logo.svg';
 import './App.css';
 import Panel from './Components/Panel'
 
-class App extends Component {
-  state = {
-    characters : 
-    ['a', 'b', 'c', 'd', 
-    'e', 'f', 'g', 'h',
-    'i', 'j', 'k', 'l']
-  }
+function App() {
+  const characters =
+    ['a', 'b', 'c', 'd',
+      'e', 'f', 'g', 'h',
+      'i', 'j', 'k', 'l']
 
-  render() {
-    return (
-      //<Panel image='./images/b.png'/>
-      //<Wrapper>
-       // <Title>Friends List</Title>
-       <div>
-        {this.state.characters.forEach(function(character) {
-          return <Panel filename={character}/>
-        })}
-        </div>
-      //</Wrapper>
-    );
-  }
+  return (
+    //<Panel image='./images/b.png'/>
+    //<Wrapper>
+    // <Title>Friends List</Title>
+    <div>
+      {characters.map(character => (
+        <Panel filename={character} />
+      ))}    
+      </div>
+    //</Wrapper>
+  );
 }
 
 export default App;
